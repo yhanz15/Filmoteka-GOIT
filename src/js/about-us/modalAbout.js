475fc2adebc16383c8db3ed0d2246179880bd9a2
yhanz15 <yhanz.kd15@gmail.com>
2024-05-21 19:30:52 +0800

    3rd commit

--- src/js/about-us/modalAbout.js
+++ src/js/about-us/modalAbout.js
@@ -1,6 +1,6 @@
 import 'swiper/swiper.scss';
 import 'swiper/modules/pagination.scss';
-import Swiper, from 'swiper';    /*{ Navigation, Pagination } */
+import Swiper, { Navigation, Pagination }  from 'swiper'; 
 import { refs } from '../constants/refs';
 
 const aboutModal = refs.aboutModal;
@@ -13,60 +13,59 @@ btnOpenAboutModal.addEventListener('click', openAboutModal);
 btnCloseAboutModal.addEventListener('click', closeAboutModal);
 
 function openAboutModal() {
-    aboutModal.classList.add('is-shown');
-    body.classList.add('modal-open');
-    refs.btnToTop.style.display = "none";
-    document.addEventListener('keydown', onCloseEsc);
-    document.addEventListener('click', function onCloseClick(e) {
-        if (e.target === aboutModal) {
-            closeAboutModal();
-        }
-    });
+  aboutModal.classList.add('is-shown');
+  body.classList.add('modal-open');
+  refs.btnToTop.style.display = 'none';
+  document.addEventListener('keydown', onCloseEsc);
+  document.addEventListener('click', function onCloseClick(e) {
+    if (e.target === aboutModal) {
+      closeAboutModal();
+    }
+  });
 }
 
 function onCloseEsc(e) {
-    if (e.code === "Escape") {
-        closeAboutModal();
-    }
+  if (e.code === 'Escape') {
+    closeAboutModal();
+  }
 }
 
 function closeAboutModal() {
-    aboutModal.classList.remove('is-shown');
-    body.classList.remove('modal-open');
-    document.removeEventListener('keydown', onCloseEsc);
-    refs.btnToTop.style.display = "block";
+  aboutModal.classList.remove('is-shown');
+  body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onCloseEsc);
+  refs.btnToTop.style.display = 'block';
 }
 
 const swipers = new Swiper(swiperModal, {
-    modules: [Navigation, Pagination],
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-      dynamicBullets: true,
-      },
-    scrollbar: {
-      el: '.swiper-scrollbar',
-      draggable: true,
-    },
-    keyboard: {
-        enabled: true,
-        onlyInViewport: true,
-        pageUpDown: true,
-    },
-    mousewheel: {
-        invert: true,
-        sensitivity: 1,
-        eventsTarget: "swiper"
-    },
-    centeredSlides: true,
-    grabCursor: true,
-    slideToClickedSlide: true,
-    loop: true,
-    slidesPerView: 'auto',
-    freeMode: true,
-
+  modules: [Navigation, Pagination],
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+  mousewheel: {
+    invert: true,
+    sensitivity: 1,
+    eventsTarget: 'swiper',
+  },
+  centeredSlides: true,
+  grabCursor: true,
+  slideToClickedSlide: true,
+  loop: true,
+  slidesPerView: 'auto',
+  freeMode: true,
 });
