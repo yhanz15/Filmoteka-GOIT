b4aac073a858228375e02d7ce96f97b5851879e6
JuliusB-ui <baraquiel.julius@gmail.com>
2024-05-25 17:46:40 +0600

    modified src/index.js to include movieSearch.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-// import './js/about-us/modalAbout';
-import { onFormSubmit } from './js/get-movies/movieSearch';
+import './js/about-us/modalAbout';
 import { refs } from './js/constants/refs';
+import { onFormSubmit } from './js/get-movies/movieSearch';
 import './js/features/upbutton/upToTop';
 
 refs.headerForm.addEventListener('submit', onFormSubmit);
